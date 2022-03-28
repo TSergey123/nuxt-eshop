e9fa9c0eff4a9f009e26654d43bb82d1d3f57093
topsv <topsv@hismith.ru>
2022-03-28 16:03:43 +0300

    gh-pages nuxt generate

---
+++ dist/_nuxt/static/1648472581/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-eshop\u002F_nuxt\u002Fstatic\u002F1648472581",layout:"default",error:a,state:{purchases:{cart:[],products:[]}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-eshop\u002F",assetsPath:"\u002Fnuxt-eshop\u002F_nuxt\u002F",cdnURL:a}}}}(null));
