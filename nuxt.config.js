565c0d7a773b0263a497efb71960af04152f66e8
topsv <topsv@hismith.ru>
2022-03-28 15:54:07 +0300

    gh-pages nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   router: {
-    base: '/<nuxt-eshop>/'
+    base: '/nuxt-eshop/'
   },
   head: {
     titleTemplate: '%s - nuxt-eshop',
